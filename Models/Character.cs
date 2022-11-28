d33cb5b21e2efdeb28eb8bd5ec2d6c5aef47833d
Saif Mohsen <saif131211@gmail.com>
2022-11-29 03:35:30 +0400

    init commit

---
+++ Models/Character.cs
@@ -0,0 +1,17 @@
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "Frodo";
+
+        public int HitPoints { get; set; } = 100;
+
+        public int Strength { get; set; } = 10;
+
+        public int Defence { get; set; } = 10;
+
+        public int Intelligance { get; set; } = 10;
+    }
+}

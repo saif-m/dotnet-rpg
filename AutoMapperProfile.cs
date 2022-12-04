8f2f56b5b5b4d663264fb54108b90fdfe9308ff3
Saif Mohsen <saif131211@gmail.com>
2022-12-04 19:41:30 +0400

    AutoMapper

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using dotnet_rpg.Dtos.Characters;
+
+namespace dotnet_rpg
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+               CreateMap<Character, GetCharacterDto>();
+               CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}

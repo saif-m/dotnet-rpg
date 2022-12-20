649459974ae6d00ec97a8e1b38704593d5e6bd0a
Saif <saif.mohsen@deg.shj.ae>
2022-12-20 09:45:03 +0400

    AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using dotnet_rpg.Data;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_rpg.Dtos.User;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _AuthRepo;
+
+        public AuthController(IAuthRepository AuthRepo)
+        {
+            _AuthRepo = AuthRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> AddUser(UserRegisterDto request)
+        {
+            var response = await _AuthRepo.Register(
+                new User{ Username = request.Username }, request.Password
+            );
+
+            if (!response.Success) {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}

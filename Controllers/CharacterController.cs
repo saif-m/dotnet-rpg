8f6f80ba8e72a20b1db55b5928c2d96a472d8b18
Saif <saif.mohsen@deg.shj.ae>
2022-12-21 14:02:16 +0400

    Testing Secured Methods with Swagger

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,9 +1,11 @@
 using dotnet_rpg.Dtos.Characters;
 using dotnet_rpg.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
 
